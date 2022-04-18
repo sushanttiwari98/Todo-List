fb51f04df08cee0b7ea6f1767befed65cb1d948d
sushanttiwari98 <sushantktiwari98@gmail.com>
2022-04-19 00:54:37 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './MyComponents/Header';
+import {Todos} from './MyComponents/Todos';
+import {Footer} from './MyComponents/Footer';
+import {AddTodo} from './MyComponents/AddTodo';
+import React, {useState} from 'react';
 
 function App() {
+  const onDelete = (todo)=>{
+    console.log("I am ondelete of todo", todo);
+    // Deleting this way in react doesnot work
+    //let index=todos.indexOf(todo);
+    //todos.splice(index,1);
+    setTodos(todos.filter((e)=>{
+      return e!==todo;
+    }));
+  }
+
+  const addTodo=(title, desc)=>{
+    console.log("I am adding this todo", title, desc);
+    let sno;
+    if(todos.length==0){
+      sno = 0;
+    }
+    else{
+    sno = todos[todos.length-1].sno +1;
+    }
+    const myTodo = {
+      sno:sno,
+      title: title,
+      desc: desc
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  }
+
+  const[todos, setTodos]=useState([
+  {
+    sno: 1,
+    title:"Go to the College",
+    desc:"You need to go to the market to get this job done"
+  },
+  {
+    sno: 2,
+    title:"Go to the Ghat",
+    desc:"You need to go to the market to get this job done 2"
+  },
+  {
+    sno: 3,
+    title:"Go to the Lanka Market",
+    desc:"You need to go to the market to get this job done 3"
+  }
+]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="My Todos List" searchBar="True"/>
+      <AddTodo addTodo={addTodo}/>
+      <Todos todos={todos} onDelete={onDelete}/>
+      <Footer/>
+      
+    </>
   );
 }
 
